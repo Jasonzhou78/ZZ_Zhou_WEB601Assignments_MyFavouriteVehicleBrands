8e9b497ef434629c37103372206a3fd73299c91b
Jasonzjzhou <zz@myscc.ca>
2022-02-06 10:18:07 -0500

    Update app.component.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app.component.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ContentList } from 'src/helper-files/content-list';
-import { Content } from '../helper-files/content-interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
