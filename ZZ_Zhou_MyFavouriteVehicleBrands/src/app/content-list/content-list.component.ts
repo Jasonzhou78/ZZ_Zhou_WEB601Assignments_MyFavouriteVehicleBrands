05b23ea993888691da165ecce9d97655b3eb4d11
Jasonzjzhou <zz@myscc.ca>
2022-02-11 12:48:31 -0500

    update tag in contentlist.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/content-list/content-list.component.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/content-list/content-list.component.ts
@@ -16,7 +16,7 @@ export class ContentListComponent implements OnInit {
       creator: 'Karl Rapp',
       imgURL: "https://lezebre.lu/images/detailed/17/30011-bmw-logo.png",
       type: 'Passenger car',
-      tag: ['BMW','BWM']},
+      tag: ['i7','535']},
     
       {id: 2,
         title: 'Volvo motors',
@@ -24,7 +24,7 @@ export class ContentListComponent implements OnInit {
         creator: 'Assar Gabrielsson',
         imgURL: "https://static.dezeen.com/uploads/2021/09/volvo_flat_logo_design_transport_dezeen_2364_col_sq_4.jpg",
         type: 'Passenger car and truck',
-        tag: ['Vol','Volvo']
+        tag: ['XC60','X70']
       },
     
       {id: 3,
@@ -33,7 +33,7 @@ export class ContentListComponent implements OnInit {
         creator: 'Kiichiro Toyoda',
         imgURL: "https://global.toyota/pages/global_toyota/mobility/toyota-brand/emblem_ogp_001.png",
         type: 'Passenger car',
-        tag: ['T','Toyota']},
+        tag: ['Camry','Sienna']},
 
       {id: 4,
         title: 'Ford motors',
@@ -41,7 +41,7 @@ export class ContentListComponent implements OnInit {
         creator: 'Henry Ford',
         imgURL: "https://www.carlogos.org/car-logos/ford-logo-2017.png",
         type: 'Passenger car and others',
-        tag: ['F','Ford']},
+        tag: ['Escape','F-150']},
 
       {id: 5,
         title: 'General motors',
@@ -49,7 +49,7 @@ export class ContentListComponent implements OnInit {
         creator: 'Willians C. Durant',
         imgURL: "https://static.dezeen.com/uploads/2021/01/gm-logo-rebrand-electric-vehicles-design_dezeen_2364_col_0-1.gif",
         type: 'Passenger car and others',
-        tag: ['GM','G']},
+        tag: ['Buick','GMC']},
 
       {id: 6,
         title: 'Audi motors',
@@ -57,7 +57,7 @@ export class ContentListComponent implements OnInit {
         creator: 'August Horch',
         imgURL: "https://logowik.com/content/uploads/images/562_audi.jpg",
         type: 'Passenger car',
-        tag: ['A','Audi']}
+        tag: ['A4','A6']}
     ];
    }
 
