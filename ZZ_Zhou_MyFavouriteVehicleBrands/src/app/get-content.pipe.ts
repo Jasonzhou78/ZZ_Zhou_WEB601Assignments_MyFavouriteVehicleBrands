6293040eae1e0854c77fbd9c16c0c73a59165598
Jasonzjzhou <zz@myscc.ca>
2022-02-13 09:01:47 -0500

    Modify custom pipe

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/get-content.pipe.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/get-content.pipe.ts
@@ -1,13 +1,15 @@
-import { isNgTemplate } from '@angular/compiler';
-import { Input, Pipe, PipeTransform } from '@angular/core';
+/* import { isNgTemplate } from '@angular/compiler'; */
+import { Pipe, PipeTransform } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentListComponent } from './content-list/content-list.component';
+/* import { ContentListComponent } from './content-list/content-list.component'; */
 @Pipe({
   name: 'getContent'
 })
 export class GetContentPipe implements PipeTransform {
-   transform(inputType?: string): [] {
-    return [];
+   transform(myList: Content[]): Content[] {
+    return myList.filter (f =>
+      f.tags != null ? f.tags.length != 0 : false
+    );
     }
     
   } 
