3022f104c4ed15930dce93a76e8490f45a934514
Jasonzjzhou <zz@myscc.ca>
2022-02-20 13:37:38 -0500

    Update content-list.component.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/content-list/content-list.component.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/content-list/content-list.component.ts
@@ -17,8 +17,6 @@ export class ContentListComponent implements OnInit {
       description: 'German brand',
       creator: 'Karl Rapp',
       imgURL: "https://lezebre.lu/images/detailed/17/30011-bmw-logo.png",
-      type: 'sport car',
-      drive: 'Four',
       tags: ['i7','535']},
     
       {id: 2,
@@ -26,8 +24,6 @@ export class ContentListComponent implements OnInit {
         description: 'Sweden brand',
         creator: 'Assar Gabrielsson',
         imgURL: "https://static.dezeen.com/uploads/2021/09/volvo_flat_logo_design_transport_dezeen_2364_col_sq_4.jpg",
-        type: 'truck',
-        drive: 'Four',
         tags: ['XC60','X70']
       },
     
@@ -36,8 +32,6 @@ export class ContentListComponent implements OnInit {
         description: 'Japanese brand',
         creator: 'Kiichiro Toyoda',
         imgURL: "https://global.toyota/pages/global_toyota/mobility/toyota-brand/emblem_ogp_001.png",
-        type: 'Passenger car',
-        drive: 'Two',
         tags: ['Camry','Sienna']},
 
       {id: 4,
@@ -45,8 +39,6 @@ export class ContentListComponent implements OnInit {
         description: 'American brand',
         creator: 'Henry Ford',
         imgURL: "https://www.carlogos.org/car-logos/ford-logo-2017.png",
-        type: 'truck',
-        drive: 'Two',
         tags: ['Escape','F-150']},
 
       {id: 5,
@@ -54,8 +46,6 @@ export class ContentListComponent implements OnInit {
         description: 'American brand',
         creator: 'Willians C. Durant',
         imgURL: "https://static.dezeen.com/uploads/2021/01/gm-logo-rebrand-electric-vehicles-design_dezeen_2364_col_0-1.gif",
-        type: 'Passenger car',
-        drive: 'Two',
         tags: ['Buick','GMC']},
 
       {id: 6,
@@ -63,8 +53,6 @@ export class ContentListComponent implements OnInit {
         description: 'German brand',
         creator: 'August Horch',
         imgURL: "https://logowik.com/content/uploads/images/562_audi.jpg",
-        type: 'sport car',
-        drive: 'Four',
         tags: ['A4','A6']},
 
       {id: 7,
@@ -72,16 +60,26 @@ export class ContentListComponent implements OnInit {
         description: 'American brand',
         creator: 'Walter P. Chrysler',
         imgURL: "https://logos-world.net/wp-content/uploads/2021/08/Chrysler-Logo-700x394.png",
-        type: 'Passenger car',
-        drive: 'Two',
         tags: ['300','dodge']}  
     ];
    }
-   get carList(){
-    return this.myList;
-  }
 
   ngOnInit(): void {
+  }
+
+  checkForTitle(searchValue: string): void{
+    let searchList = this.myList.filter(c => c.title == searchValue);
+    if (searchList.length > 0){
+      this.searchMessage  = "Found the car!";
+      this.searchFlag = true;
+    }
+    else{
+      this.searchMessage  = "No car with that title";
+      this.searchFlag = false;
+    }
+  }
+  donothing(){
+
   }
 
 }
