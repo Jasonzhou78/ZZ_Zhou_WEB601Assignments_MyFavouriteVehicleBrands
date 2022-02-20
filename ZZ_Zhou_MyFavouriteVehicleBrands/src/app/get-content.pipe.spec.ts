9bad8df6cabd395397d42bb9856ed1654bde3c44
Jasonzjzhou <zz@myscc.ca>
2022-02-20 15:05:50 -0500

    recheck the output

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/get-content.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetContentPipe } from './get-content.pipe';
-
-describe('GetContentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetContentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
