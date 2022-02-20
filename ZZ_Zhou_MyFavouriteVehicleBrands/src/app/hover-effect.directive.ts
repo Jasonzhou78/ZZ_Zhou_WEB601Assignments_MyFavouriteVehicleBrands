5b4080e0943f59e7e4a706deb063d76def3ec177
Jasonzjzhou <zz@myscc.ca>
2022-02-20 17:36:49 -0500

    modify directive

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/hover-effect.directive.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/hover-effect.directive.ts
@@ -1,11 +1,29 @@
 import { Directive, ElementRef } from '@angular/core';
-
+import { HostListener } from '@angular/core'; 
 @Directive({
   selector: '[appHoverEffect]'
 })
 export class HoverEffectDirective {
   constructor(private el:ElementRef) { 
-    this.el.nativeElement.style.hover = 'yellow';
+
+   }
+   
+   @HostListener('mouseover')
+   onMouseOver() {
+    
+    let tag = this.el.nativeElement.tagName;
+    if (tag.toLowerCase() == 'div')
+    this.el.nativeElement.style.textDecoration = 'underline';
+    else if(tag.toLowerCase() == 'span')
+    this.el.nativeElement.style.fontWeight = 'bold'; 
+   }
+ 
+   @HostListener('mouseout')
+   onMouseOut() {
+    if (this.el.nativeElement.tagName.toLowerCase() == 'div')
+    this.el.nativeElement.style.textDecoration =  '';
+    else if(this.el.nativeElement.tagName.toLowerCase() == 'span')
+    this.el.nativeElement.style.fontWeight = 'normal';
    }
 
 }
