7ce7538ac950a51d18b45ad648ae2e72e565a653
Jason <90533925+Jasonzhou78@users.noreply.github.com>
2022-03-03 18:50:36 -0500

    Update title-filter.pipe.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/title-filter.pipe.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/title-filter.pipe.ts
@@ -1,22 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 @Pipe({
-  name: 'titleFilter'
+  name: 'titleFilter',
 })
 export class TitleFilterPipe implements PipeTransform {
-
   transform(contentList: Content[], filterByThisType?: string): Content[] {
-    console.log("filter value: ", filterByThisType);
+    console.log('filter value: ', filterByThisType);
     // return contentList.filter(c => c.type == filterByThisType);
     return contentList.filter(checkType);
-
-    function checkType(contentList) {
-    if (filterBythisType) {
-      return contentList.type == filterByThisType;
-    } else {
-      return !contentList.type;
-    }
+    function checkType(contentList: Content) {
+      if (filterByThisType) {
+        return contentList.type == filterByThisType;
+      } else {
+        return !filterByThisType;
+      }
     }
   }
-
 }
