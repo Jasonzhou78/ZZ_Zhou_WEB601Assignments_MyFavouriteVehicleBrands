e7d072cea8364fbc57227ae0058a97ebdeaf3b0a
Jason <90533925+Jasonzhou78@users.noreply.github.com>
2022-03-24 14:11:51 -0400

    change the name of ContentDb to contentDB
    Linux is case sensitive

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/services/content.service.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/services/content.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Content } from '../../helper-files/content-interface';
 import { Observable, of } from 'rxjs';
-import { LISTOFCARS } from '../../helper-files/ContentDb';
+import { LISTOFCARS } from '../../helper-files/contentDB';
 
 @Injectable({
   providedIn: 'root'
