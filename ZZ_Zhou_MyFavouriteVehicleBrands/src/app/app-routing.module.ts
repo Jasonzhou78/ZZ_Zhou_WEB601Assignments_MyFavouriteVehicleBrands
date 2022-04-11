87bdf92871cbaf23b3b5b942820792235990c98e
Jasonzjzhou <zz@myscc.ca>
2022-04-10 22:19:27 -0400

    Update app-routing.module.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app-routing.module.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ContentListComponent } from '../app/content-list/content-list.component';
+import { ContentDetailComponent } from '../app/content-detail/content-detail.component';
 
 
+const routes: Routes = [
+  {
+    path:"content",
+    component: ContentListComponent,
+  },
+  {
+    path:'detail',
+    component: ContentDetailComponent,
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
