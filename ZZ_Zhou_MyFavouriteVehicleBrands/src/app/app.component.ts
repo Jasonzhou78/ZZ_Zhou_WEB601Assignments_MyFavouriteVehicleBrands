8137cabb451d2a0940d43400bd23b87dac68346a
Jasonzjzhou <zz@myscc.ca>
2022-04-10 22:26:56 -0400

    Update app.component.ts

--- ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app.component.ts
+++ ZZ_Zhou_MyFavouriteVehicleBrands/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { CarService } from '../app/services/content.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { 
-  title = 'ZZ_Zhou Favourite Vehicle';
+export class AppComponent implements OnInit{ 
+  title = 'string';
+  constructor(private carService: CarService) {
+  this.title = "header 1";
+  }
   ngOnInit(): void {
   }
 }
-
